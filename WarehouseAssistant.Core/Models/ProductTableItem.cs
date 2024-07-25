90e8f6d6e6b30d6c519cd18c65b077d54395b2cf
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-07-25 13:37:29 +0300

    Table Builder and Attributes
    * Added WorkbookBuilder<T> class for generating xlsx tables.
    * Added attributes to properties of ProductTableItem class for correct operation of MiniExcel package
    * Added tests for WorkbookBuilder<T>

--- WarehouseAssistant.Core/Models/ProductTableItem.cs
+++ WarehouseAssistant.Core/Models/ProductTableItem.cs
@@ -1,19 +1,37 @@
-﻿using WarehouseAssistant.Core.Calculation;
+﻿using MiniExcelLibs.Attributes;
+using WarehouseAssistant.Core.Calculation;
 
 namespace WarehouseAssistant.Core.Models;
 
 public class ProductTableItem : ICalculationData
 {
     private int _quantityToOrder;
-    public required string Name              { get; set; }
-    public required int    Article           { get; set; }
-    public int    AvailableQuantity { get; set; }
-    public int    CurrentQuantity   { get; set; }
-    public int    Reserved          { get; set; }
-    public double AverageTurnover   { get; set; }
-    public double StockDays         { get; set; }
-    public double OrderCalculation  { get; set; }
 
+    [ExcelColumn(Name = "Название", Aliases = ["Номенклатура"], Width = 70.0)]
+    public required string Name { get; set; }
+
+    [ExcelColumn(Name = "Артикул", Width = 10.0)]
+    public required int Article { get; set; }
+
+    [ExcelColumn(Name = "Доступно на БГЛЦ", Aliases = ["Доступно основной склад МО"], Width = 16.0)]
+    public int AvailableQuantity { get; set; }
+
+    [ExcelColumn(Name = "Текущее количество", Aliases = ["Доступно Санкт-Петербург (склад)"], Width = 18.0)]
+    public int CurrentQuantity { get; set; }
+
+    [ExcelIgnore]
+    public int Reserved { get; set; }
+
+    [ExcelColumn(Name = "Средний расход в день", Aliases = ["Средняя оборачиваемость в день"], Width = 16.0)]
+    public double AverageTurnover { get; set; }
+
+    [ExcelColumn(Name = "Запас товара (на кол-во дней)", Width = 17.0)]
+    public double StockDays { get; set; }
+
+    [ExcelColumn(Name = "Рекомендуемое количество", Aliases = ["Расчет заказа"], Width = 13.0)]
+    public double OrderCalculation { get; set; }
+
+    [ExcelColumn(Name = "Нужно заказать", Width = 16.0, Aliases = ["Заказ на офис Спб"])]
     public int QuantityToOrder
     {
         get => _quantityToOrder;
---
+++ WarehouseAssistant.Core/Services/WorkbookBuilder.cs
@@ -0,0 +1,52 @@
+﻿using MiniExcelLibs;
+
+namespace WarehouseAssistant.Core.Services;
+
+// Должен создавать и сохранять таблицы на диске.
+public sealed class WorkbookBuilder<T> : IDisposable, IAsyncDisposable
+{
+    private readonly MemoryStream                _workbookStream = new();
+    private readonly Dictionary<string, List<T>> _sheets         = [];
+
+    public byte[] AsByteArray()
+    {
+        return AsByteArray(null);
+    }
+
+    public byte[] AsByteArray(IConfiguration? configuration)
+    {
+        Dictionary<string, object> sheets = _sheets.ToDictionary<KeyValuePair<string, List<T>>, string, object>(keyValuePair => keyValuePair.Key, keyValuePair => keyValuePair.Value);
+
+        _workbookStream.SaveAs(sheets, configuration: configuration);
+        return _workbookStream.ToArray();
+    }
+
+    public bool CreateSheet(string sheetName)
+    {
+        if (_sheets.ContainsKey(sheetName))
+            return false;
+
+        _sheets.Add(sheetName, []);
+        return true;
+    }
+
+    public void AddToSheet(string sheetName, T data)
+    {
+        _sheets[sheetName].Add(data);
+    }
+
+    public void AddRangeToSheet(string sheetName, IEnumerable<T> data)
+    {
+        _sheets[sheetName].AddRange(data);
+    }
+
+    public void Dispose()
+    {
+        _workbookStream.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _workbookStream.DisposeAsync();
+    }
+}
