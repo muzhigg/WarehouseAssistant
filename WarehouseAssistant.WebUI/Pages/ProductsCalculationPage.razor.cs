7f31318afb7b872cfdce4216663ec2413d1d8163
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-07-26 15:26:44 +0300

    start of work on the products calculation module
    table was added to the page, which in the future will contain products from excel table

---
+++ WarehouseAssistant.WebUI/Pages/ProductsCalculationPage.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WarehouseAssistant.Core.Models;
+
+namespace WarehouseAssistant.WebUI.Pages;
+
+public partial class ProductsCalculationPage : ComponentBase
+{
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private IEnumerable<ProductTableItem> _products     = new List<ProductTableItem>();
+    private string                        _searchString = null!;
+
+    private bool ShouldDisplayProduct(ProductTableItem arg)
+    {
+        if (string.IsNullOrWhiteSpace(_searchString))
+            return true;
+
+        if (arg.Article.ToString().Contains(_searchString)) return true;
+
+        if (arg.Name.Contains(_searchString, StringComparison.OrdinalIgnoreCase)) return true;
+
+        return false;
+    }
+
+    private void OnQuantityToOrderCommittedChanges(ProductTableItem obj)
+    {
+        if (obj.QuantityToOrder < 0)
+        {
+            Snackbar.Add("Значение не может быть меньше нуля", Severity.Error);
+            obj.QuantityToOrder = 0;
+        }
+    }
+}
