cf3f87adcf6c2816a1b4d12fa1857f4c9a2ed99e
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-10-20 02:15:10 +0300

    start of work on the module for receiving goods
    - page created and data model added

---
+++ WarehouseAssistant.WebUI/Pages/ReceivingPage.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+using WarehouseAssistant.Shared.Models;
+
+namespace WarehouseAssistant.WebUI.Pages;
+
+public partial class ReceivingPage : ComponentBase
+{
+    private List<ReceivingItem> _receivingItems;
+}
