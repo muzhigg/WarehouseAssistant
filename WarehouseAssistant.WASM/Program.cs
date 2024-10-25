2b889e69bacaa9f3ef5d1d822ba93139709bfc61
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-10-25 22:46:57 +0300

    Added a single Routes.razor component for all applications

--- WarehouseAssistant.WASM/Program.cs
+++ WarehouseAssistant.WASM/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WarehouseAssistant.WASM;
 using WarehouseAssistant.WebUI;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
