50a6f97cecc41cf0af1d5d0b57ab30439c2f64a9
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-10-27 16:17:58 +0300

    Added mocking and dependency injection of AuthenticationStateProvider to support role-based access control. Updated the initialization logic of the ProductFormDialog component to check user roles and control access based on role permissions.

--- WarehouseAssistant.WebUI.Tests/MudBlazorTestContext.cs
+++ WarehouseAssistant.WebUI.Tests/MudBlazorTestContext.cs
@@ -1,4 +1,7 @@
 ﻿using System.Net.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using Moq;
 using MudBlazor;
 using MudBlazor.Services;
 
@@ -6,6 +9,8 @@ namespace WarehouseAssistant.WebUI.Tests;
 
 public class MudBlazorTestContext : TestContext
 {
+    private Mock<AuthenticationStateProvider> _authenticationStateProvider = new Mock<AuthenticationStateProvider>();
+    
     protected MudBlazorTestContext()
     {
         JSInterop.Mode = JSRuntimeMode.Loose;
@@ -24,6 +29,11 @@ public class MudBlazorTestContext : TestContext
             .AddMudPopoverService()
             .AddMudEventManager()
             .AddMudLocalization();
+        Services.AddSingleton(_authenticationStateProvider.Object);
+        
+        _authenticationStateProvider.Setup(provider => provider.GetAuthenticationStateAsync())
+            .ReturnsAsync(new AuthenticationState(
+                new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Role, "Admin") }, "jwt"))));
     }
     
     protected IRenderedComponent<MudDialogProvider> RenderedDialogProvider(out DialogService? service)
