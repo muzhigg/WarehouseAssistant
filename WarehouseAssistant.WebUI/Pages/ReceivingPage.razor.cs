91fb81d67ce52327dbcb0fc47e916c7bd8927246
muzhigg <88670581+muzhigg@users.noreply.github.com>
2024-10-27 19:11:10 +0300

    Update receive page with autofocus, async submission, and new barcode dialog
    Updated receive input form to add autofocus;
    changed submission processing to asynchronous;
    added new barcode input dialog and corresponding logic to handle unfound items on the receive page.
    Remove unused code and sound playback logic.

--- WarehouseAssistant.WebUI/Pages/ReceivingPage.razor.cs
+++ WarehouseAssistant.WebUI/Pages/ReceivingPage.razor.cs
@@ -8,6 +8,7 @@ using WarehouseAssistant.Data.Repositories;
 using WarehouseAssistant.Shared.Models;
 using WarehouseAssistant.Shared.Models.Db;
 using WarehouseAssistant.WebUI.Components;
+using WarehouseAssistant.WebUI.DatabaseModule;
 using WarehouseAssistant.WebUI.Models;
 using Timer = System.Timers.Timer;
 
@@ -16,23 +17,25 @@ namespace WarehouseAssistant.WebUI.Pages;
 [UsedImplicitly]
 public partial class ReceivingPage : ComponentBase, IDisposable
 {
-    [Inject] public IRepository<ReceivingItem> Repository        { get; set; } = null!;
-    [Inject] public IRepository<Product>       ProductRepository { get; set; } = null!;
-    [Inject] public ISnackbar                  Snackbar          { get; set; } = null!;
-    [Inject] public IJSRuntime                 JsRuntime         { get; set; } = null!;
-    [Inject] public IDialogService             DialogService     { get; set; } = null!;
+    [Inject] public  IRepository<ReceivingItem> Repository        { get; set; } = null!;
+    [Inject] public  IRepository<Product>       ProductRepository { get; set; } = null!;
+    [Inject] public  ISnackbar                  Snackbar          { get; set; } = null!;
+    [Inject] public  IJSRuntime                 JsRuntime         { get; set; } = null!;
+    [Inject] public  IDialogService             DialogService     { get; set; } = null!;
+    [Inject] private IProductFormDialogService  ProductFormDialog { get; set; } = null!;
     
     private Table<ReceivingItem> _table = null!;
     private bool                 _isBusy;
     
-    private List<Product>? _dbProducts;
-    private Timer          _timer = new Timer(300000);
+    private          List<Product>? _dbProducts;
+    private readonly Timer          _timer           = new Timer(300000);
+    private          MudMessageBox  _articleInputBox = null!;
+    private          string?        _articleInput;
     
     protected override async Task OnInitializedAsync()
     {
         _timer.Elapsed   += TimerOnElapsed;
         _timer.AutoReset =  true;
-        
         
         try
         {
@@ -67,53 +70,14 @@ public partial class ReceivingPage : ComponentBase, IDisposable
         Snackbar.Add("Данные сохранены.", Severity.Success);
     }
     
-    // protected override async Task OnAfterRenderAsync(bool firstRender)
-    // {
-    //     if (firstRender)
-    //     {
-    //         
-    //         
-    //         try
-    //         {
-    //             
-    //         }
-    //         catch (Exception e)
-    //         {
-    //             Snackbar.Add($"Ошибка при обращении к базе данных: {e.Message}", Severity.Error);
-    //         }
-    //         finally
-    //         {
-    //             
-    //         }
-    //     }
-    //     
-    //     await base.OnAfterRenderAsync(firstRender);
-    // }
-    
-    private void OnInputProvided(ReceivingInputData obj)
+    private async Task OnInputProvided(ReceivingInputData obj)
     {
-        Receive(obj);
+        _isBusy = true;
+        await Receive(obj);
+        _isBusy = false;
+        StateHasChanged();
     }
     
-    private enum NotificationSoundType
-    {
-        Success,
-        Error,
-        Warning
-    }
-    
-    private async Task PlaySound(NotificationSoundType soundType)
-    {
-        string soundUrl = soundType switch
-        {
-            NotificationSoundType.Success => "_content/WarehouseAssistant.WebUI/sounds/success.mp3",
-            NotificationSoundType.Error   => "_content/WarehouseAssistant.WebUI/sounds/error.wav",
-            NotificationSoundType.Warning => "_content/WarehouseAssistant.WebUI/sounds/warning.mp3",
-            _                             => throw new ArgumentOutOfRangeException(nameof(soundType), soundType, null)
-        };
-        
-        await JsRuntime.InvokeVoidAsync("playNotificationSound", soundUrl);
-    }
     
     internal async Task Receive(ReceivingInputData obj)
     {
@@ -135,11 +99,54 @@ public partial class ReceivingPage : ComponentBase, IDisposable
             }
         }
         
+        if (tableItem == null && obj.Id.Length > 10)
+        {
+            bool? confirmed = await _articleInputBox.Show();
+            if (confirmed is true && string.IsNullOrEmpty(_articleInput) == false)
+            {
+                if (_dbProducts != null)
+                {
+                    if (_dbProducts.Any(product => product.Article == _articleInput))
+                    {
+                        var dbProduct = _dbProducts.First(product => product.Article == _articleInput);
+                        dbProduct.Barcode = obj.Id;
+                        try
+                        {
+                            await ProductRepository.UpdateAsync(dbProduct);
+                        }
+                        catch (HttpRequestException e)
+                        {
+                            Snackbar.Add($"Ошибка при обращении к базе данных: {e.Message}", Severity.Error);
+                            Debug.WriteLine(e);
+                        }
+                        
+                        tableItem = _table.Items.FirstOrDefault(item => item.Article == _articleInput);
+                    }
+                    else
+                    {
+                        tableItem = _table.Items.FirstOrDefault(item => item.Article == _articleInput);
+                        
+                        if (tableItem != null)
+                        {
+                            Product dbProduct = new Product()
+                            {
+                                Article = _articleInput,
+                                Barcode = obj.Id,
+                                Name    = tableItem.Name,
+                            };
+                            var success = await ProductFormDialog.ShowAddDialogAsync(dbProduct);
+                            
+                            if (success)
+                                _dbProducts.Add(dbProduct);
+                        }
+                    }
+                }
+            }
+        }
+        
         if (tableItem == null)
         {
-            // TODO Play Error sound
             Snackbar.Add($"Не удалось найти товар с идентификатором {obj.Id}.", Severity.Error);
-            PlaySound(NotificationSoundType.Error);
             return;
         }
         
@@ -147,7 +154,6 @@ public partial class ReceivingPage : ComponentBase, IDisposable
         
         if (tableItem.ReceivedQuantity < tableItem.ExpectedQuantity)
         {
-            // TODO Play Info sound
             Snackbar.Add(
                 $"Товар {tableItem.Article} получено {obj.Quantity} шт. Осталось {tableItem.ExpectedQuantity - tableItem.ReceivedQuantity} шт.",
                 Severity.Info);
@@ -165,9 +171,6 @@ public partial class ReceivingPage : ComponentBase, IDisposable
     
     private async Task CompleteReceiving()
     {
-        // TODO: Complete receiving session and save data to database.
-        // TODO: Play Success sound
-        
         bool? confirmed = await DialogService.ShowMessageBox("Вы уверены?",
             "Приёмка будет завершена. Все накопленные данные будут удалены. Продолжить?", yesText: "Да", noText: "Нет");
         
